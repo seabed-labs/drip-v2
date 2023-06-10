ec65be250cc4e806a490a6622bca9a030f48124a
matcha <matcha@dcaf.so>
2023-06-10 18:03:57 -0400

    [sdk] feat: Leverage Anchor.toml to auto-generate IDL inside SDK

---
+++ sdk/src/idl/drip_v2.ts
@@ -0,0 +1,575 @@
+export type DripV2 = {
+  "version": "0.1.0",
+  "name": "drip_v2",
+  "instructions": [
+    {
+      "name": "initGlobalConfig",
+      "accounts": [
+        {
+          "name": "payer",
+          "isMut": true,
+          "isSigner": true
+        },
+        {
+          "name": "globalConfig",
+          "isMut": true,
+          "isSigner": true
+        },
+        {
+          "name": "systemProgram",
+          "isMut": false,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "params",
+          "type": {
+            "defined": "InitGlobalConfigParams"
+          }
+        }
+      ]
+    },
+    {
+      "name": "updateSuperAdmin",
+      "accounts": [
+        {
+          "name": "signer",
+          "isMut": false,
+          "isSigner": true
+        },
+        {
+          "name": "globalConfig",
+          "isMut": true,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "params",
+          "type": {
+            "defined": "UpdateSuperAdminParams"
+          }
+        }
+      ]
+    },
+    {
+      "name": "updateAdmin",
+      "accounts": [
+        {
+          "name": "signer",
+          "isMut": false,
+          "isSigner": true
+        },
+        {
+          "name": "globalConfig",
+          "isMut": true,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "params",
+          "type": {
+            "defined": "UpdateAdminParams"
+          }
+        }
+      ]
+    },
+    {
+      "name": "updateDefaultDripFees",
+      "accounts": [
+        {
+          "name": "signer",
+          "isMut": false,
+          "isSigner": true
+        },
+        {
+          "name": "globalConfig",
+          "isMut": true,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "params",
+          "type": {
+            "defined": "UpdateDefaultDripFeesParams"
+          }
+        }
+      ]
+    }
+  ],
+  "accounts": [
+    {
+      "name": "globalConfig",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "version",
+            "type": "u64"
+          },
+          {
+            "name": "superAdmin",
+            "type": "publicKey"
+          },
+          {
+            "name": "admins",
+            "type": {
+              "array": [
+                "publicKey",
+                20
+              ]
+            }
+          },
+          {
+            "name": "adminPermissions",
+            "type": {
+              "array": [
+                "u64",
+                20
+              ]
+            }
+          },
+          {
+            "name": "defaultDripFeeBps",
+            "type": "u64"
+          }
+        ]
+      }
+    }
+  ],
+  "types": [
+    {
+      "name": "InitGlobalConfigParams",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "version",
+            "type": "u64"
+          },
+          {
+            "name": "superAdmin",
+            "type": "publicKey"
+          },
+          {
+            "name": "defaultDripFeeBps",
+            "type": "u64"
+          }
+        ]
+      }
+    },
+    {
+      "name": "UpdateAdminParams",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "adminIndex",
+            "type": "u64"
+          },
+          {
+            "name": "adminChange",
+            "type": {
+              "defined": "AdminChange"
+            }
+          }
+        ]
+      }
+    },
+    {
+      "name": "UpdateDefaultDripFeesParams",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "newDefaultDripFeesBps",
+            "type": "u64"
+          }
+        ]
+      }
+    },
+    {
+      "name": "UpdateSuperAdminParams",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "newSuperAdmin",
+            "type": "publicKey"
+          }
+        ]
+      }
+    },
+    {
+      "name": "AdminChange",
+      "type": {
+        "kind": "enum",
+        "variants": [
+          {
+            "name": "Clear"
+          },
+          {
+            "name": "SetAdminAndResetPermissions",
+            "fields": [
+              "publicKey"
+            ]
+          },
+          {
+            "name": "ResetPermissions"
+          },
+          {
+            "name": "AddPermission",
+            "fields": [
+              {
+                "defined": "AdminPermission"
+              }
+            ]
+          },
+          {
+            "name": "RemovePermission",
+            "fields": [
+              {
+                "defined": "AdminPermission"
+              }
+            ]
+          }
+        ]
+      }
+    },
+    {
+      "name": "AdminPermission",
+      "type": {
+        "kind": "enum",
+        "variants": [
+          {
+            "name": "InitPairConfig"
+          },
+          {
+            "name": "Drip"
+          },
+          {
+            "name": "UpdateDefaultDripFees"
+          }
+        ]
+      }
+    }
+  ],
+  "errors": [
+    {
+      "code": 6000,
+      "name": "SuperAdminSignatureRequired",
+      "msg": "Signer is not super_admin"
+    },
+    {
+      "code": 6001,
+      "name": "AdminIndexOutOfBounds",
+      "msg": "Admin index out of bounds"
+    },
+    {
+      "code": 6002,
+      "name": "AdminPubkeyCannotBeDefault",
+      "msg": "SuperAdmin/Admin pubkey cannot be default"
+    },
+    {
+      "code": 6003,
+      "name": "FailedToConvertU64toUSize",
+      "msg": "Failed to convert u64 to usize"
+    },
+    {
+      "code": 6004,
+      "name": "OperationUnauthorized",
+      "msg": "Unauthorized; Requires admin permission for this op or super admin signature"
+    }
+  ]
+};
+
+export const IDL: DripV2 = {
+  "version": "0.1.0",
+  "name": "drip_v2",
+  "instructions": [
+    {
+      "name": "initGlobalConfig",
+      "accounts": [
+        {
+          "name": "payer",
+          "isMut": true,
+          "isSigner": true
+        },
+        {
+          "name": "globalConfig",
+          "isMut": true,
+          "isSigner": true
+        },
+        {
+          "name": "systemProgram",
+          "isMut": false,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "params",
+          "type": {
+            "defined": "InitGlobalConfigParams"
+          }
+        }
+      ]
+    },
+    {
+      "name": "updateSuperAdmin",
+      "accounts": [
+        {
+          "name": "signer",
+          "isMut": false,
+          "isSigner": true
+        },
+        {
+          "name": "globalConfig",
+          "isMut": true,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "params",
+          "type": {
+            "defined": "UpdateSuperAdminParams"
+          }
+        }
+      ]
+    },
+    {
+      "name": "updateAdmin",
+      "accounts": [
+        {
+          "name": "signer",
+          "isMut": false,
+          "isSigner": true
+        },
+        {
+          "name": "globalConfig",
+          "isMut": true,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "params",
+          "type": {
+            "defined": "UpdateAdminParams"
+          }
+        }
+      ]
+    },
+    {
+      "name": "updateDefaultDripFees",
+      "accounts": [
+        {
+          "name": "signer",
+          "isMut": false,
+          "isSigner": true
+        },
+        {
+          "name": "globalConfig",
+          "isMut": true,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "params",
+          "type": {
+            "defined": "UpdateDefaultDripFeesParams"
+          }
+        }
+      ]
+    }
+  ],
+  "accounts": [
+    {
+      "name": "globalConfig",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "version",
+            "type": "u64"
+          },
+          {
+            "name": "superAdmin",
+            "type": "publicKey"
+          },
+          {
+            "name": "admins",
+            "type": {
+              "array": [
+                "publicKey",
+                20
+              ]
+            }
+          },
+          {
+            "name": "adminPermissions",
+            "type": {
+              "array": [
+                "u64",
+                20
+              ]
+            }
+          },
+          {
+            "name": "defaultDripFeeBps",
+            "type": "u64"
+          }
+        ]
+      }
+    }
+  ],
+  "types": [
+    {
+      "name": "InitGlobalConfigParams",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "version",
+            "type": "u64"
+          },
+          {
+            "name": "superAdmin",
+            "type": "publicKey"
+          },
+          {
+            "name": "defaultDripFeeBps",
+            "type": "u64"
+          }
+        ]
+      }
+    },
+    {
+      "name": "UpdateAdminParams",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "adminIndex",
+            "type": "u64"
+          },
+          {
+            "name": "adminChange",
+            "type": {
+              "defined": "AdminChange"
+            }
+          }
+        ]
+      }
+    },
+    {
+      "name": "UpdateDefaultDripFeesParams",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "newDefaultDripFeesBps",
+            "type": "u64"
+          }
+        ]
+      }
+    },
+    {
+      "name": "UpdateSuperAdminParams",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "newSuperAdmin",
+            "type": "publicKey"
+          }
+        ]
+      }
+    },
+    {
+      "name": "AdminChange",
+      "type": {
+        "kind": "enum",
+        "variants": [
+          {
+            "name": "Clear"
+          },
+          {
+            "name": "SetAdminAndResetPermissions",
+            "fields": [
+              "publicKey"
+            ]
+          },
+          {
+            "name": "ResetPermissions"
+          },
+          {
+            "name": "AddPermission",
+            "fields": [
+              {
+                "defined": "AdminPermission"
+              }
+            ]
+          },
+          {
+            "name": "RemovePermission",
+            "fields": [
+              {
+                "defined": "AdminPermission"
+              }
+            ]
+          }
+        ]
+      }
+    },
+    {
+      "name": "AdminPermission",
+      "type": {
+        "kind": "enum",
+        "variants": [
+          {
+            "name": "InitPairConfig"
+          },
+          {
+            "name": "Drip"
+          },
+          {
+            "name": "UpdateDefaultDripFees"
+          }
+        ]
+      }
+    }
+  ],
+  "errors": [
+    {
+      "code": 6000,
+      "name": "SuperAdminSignatureRequired",
+      "msg": "Signer is not super_admin"
+    },
+    {
+      "code": 6001,
+      "name": "AdminIndexOutOfBounds",
+      "msg": "Admin index out of bounds"
+    },
+    {
+      "code": 6002,
+      "name": "AdminPubkeyCannotBeDefault",
+      "msg": "SuperAdmin/Admin pubkey cannot be default"
+    },
+    {
+      "code": 6003,
+      "name": "FailedToConvertU64toUSize",
+      "msg": "Failed to convert u64 to usize"
+    },
+    {
+      "code": 6004,
+      "name": "OperationUnauthorized",
+      "msg": "Unauthorized; Requires admin permission for this op or super admin signature"
+    }
+  ]
+};
--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -1,84 +1,92 @@
 import {
-  Connection,
-  Keypair,
-  PublicKey,
-  SystemProgram,
-  Transaction,
+    Connection,
+    Keypair,
+    PublicKey,
+    SystemProgram,
+    Transaction,
 } from "@solana/web3.js";
 import * as anchor from "@coral-xyz/anchor";
-import { DripV2, IDL } from "./idl";
+import { DripV2, IDL } from "./idl/drip_v2";
 import NodeWallet from "@coral-xyz/anchor/dist/cjs/nodewallet";
 import { AnchorProvider } from "@coral-xyz/anchor";
 
 export interface GlobalConfigAccount {
-  version: bigint;
-  superAdmin: PublicKey;
-  admins: PublicKey[];
-  adminPermissions: BigInt[];
-  defaultDripFeeBps: bigint;
+    version: bigint;
+    superAdmin: PublicKey;
+    admins: PublicKey[];
+    adminPermissions: BigInt[];
+    defaultDripFeeBps: bigint;
 }
 
 export class Drip {
-  private readonly program;
+    private readonly program;
 
-  public constructor(
-    public readonly programId: PublicKey,
-    public readonly providerOrConnection: AnchorProvider | Connection
-  ) {
-    const provider =
-      providerOrConnection instanceof AnchorProvider
-        ? providerOrConnection
-        : new anchor.AnchorProvider(
-            providerOrConnection,
-            new NodeWallet(Keypair.generate()),
-            { commitment: providerOrConnection.commitment }
-          );
+    public constructor(
+        public readonly programId: PublicKey,
+        public readonly providerOrConnection: AnchorProvider | Connection
+    ) {
+        const provider =
+            providerOrConnection instanceof AnchorProvider
+                ? providerOrConnection
+                : new anchor.AnchorProvider(
+                      providerOrConnection,
+                      new NodeWallet(Keypair.generate()),
+                      { commitment: providerOrConnection.commitment }
+                  );
 
-    this.program = new anchor.Program<DripV2>(IDL, this.programId, provider);
-  }
-
-  public async fetchGlobalConfig(key: PublicKey): Promise<GlobalConfigAccount> {
-    const account = await this.program.account.globalConfig.fetch(key);
-
-    return {
-      version: BigInt(account.version.toString()),
-      superAdmin: account.superAdmin,
-      admins: account.admins,
-      adminPermissions: account.adminPermissions.map((permission) =>
-        BigInt(permission.toString())
-      ),
-      defaultDripFeeBps: BigInt(account.defaultDripFeeBps.toString()),
-    };
-  }
-
-  public async initGlobalConfig(
-    version: number,
-    superAdmin: PublicKey,
-    defaultDripFeeBps: BigInt,
-    globalConfigPubkey?: PublicKey,
-    payer?: PublicKey
-  ): Promise<{ tx: Transaction; globalConfigPubkey: PublicKey }> {
-    const globalConfigKeypair = Keypair.generate();
-
-    let txBuilder = this.program.methods
-      .initGlobalConfig({
-        version: new anchor.BN(version.toString()),
-        superAdmin: superAdmin,
-        defaultDripFeeBps: new anchor.BN(defaultDripFeeBps.toString()),
-      })
-      .accounts({
-        payer: payer ?? this.program.provider.publicKey,
-        globalConfig: globalConfigPubkey ?? globalConfigKeypair.publicKey,
-        systemProgram: SystemProgram.programId,
-      });
-
-    if (!globalConfigPubkey) {
-      txBuilder = txBuilder.signers([globalConfigKeypair]);
+        this.program = new anchor.Program<DripV2>(
+            IDL,
+            this.programId,
+            provider
+        );
     }
 
-    return {
-      tx: await txBuilder.transaction(),
-      globalConfigPubkey: globalConfigPubkey ?? globalConfigKeypair.publicKey,
-    };
-  }
+    public async fetchGlobalConfig(
+        key: PublicKey
+    ): Promise<GlobalConfigAccount> {
+        const account = await this.program.account.globalConfig.fetch(key);
+
+        return {
+            version: BigInt(account.version.toString()),
+            superAdmin: account.superAdmin,
+            admins: account.admins,
+            adminPermissions: account.adminPermissions.map((permission) =>
+                BigInt(permission.toString())
+            ),
+            defaultDripFeeBps: BigInt(account.defaultDripFeeBps.toString()),
+        };
+    }
+
+    public async initGlobalConfig(
+        version: number,
+        superAdmin: PublicKey,
+        defaultDripFeeBps: BigInt,
+        globalConfigPubkey?: PublicKey,
+        payer?: PublicKey
+    ): Promise<{ tx: Transaction; globalConfigPubkey: PublicKey }> {
+        const globalConfigKeypair = Keypair.generate();
+
+        let txBuilder = this.program.methods
+            .initGlobalConfig({
+                version: new anchor.BN(version.toString()),
+                superAdmin: superAdmin,
+                defaultDripFeeBps: new anchor.BN(defaultDripFeeBps.toString()),
+            })
+            .accounts({
+                payer: payer ?? this.program.provider.publicKey,
+                globalConfig:
+                    globalConfigPubkey ?? globalConfigKeypair.publicKey,
+                systemProgram: SystemProgram.programId,
+            });
+
+        if (!globalConfigPubkey) {
+            txBuilder = txBuilder.signers([globalConfigKeypair]);
+        }
+
+        return {
+            tx: await txBuilder.transaction(),
+            globalConfigPubkey:
+                globalConfigPubkey ?? globalConfigKeypair.publicKey,
+        };
+    }
 }
