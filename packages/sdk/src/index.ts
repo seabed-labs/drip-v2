79d71d304bd75f4af2b5df4eaa6efd0852df2db5
Mocha <mocha@dcaf.so>
2023-06-17 16:48:12 -0400

    feat(root): init packages/drip-types (#26)

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,101 +1,92 @@
 import {
-    Connection,
-    Keypair,
-    PublicKey,
-    SystemProgram,
-    Transaction,
+  Connection,
+  Keypair,
+  PublicKey,
+  SystemProgram,
+  Transaction,
 } from "@solana/web3.js";
 import * as anchor from "@coral-xyz/anchor";
-import { DripV2, IDL } from "./idl/drip_v2";
+import { DripV2, IDL } from "@dcaf/drip-types";
 import NodeWallet from "@coral-xyz/anchor/dist/cjs/nodewallet";
 import { AnchorProvider } from "@coral-xyz/anchor";
 
 export interface GlobalConfigAccount {
-    version: bigint;
-    superAdmin: PublicKey;
-    admins: PublicKey[];
-    adminPermissions: BigInt[];
-    defaultDripFeeBps: bigint;
-    globalConfigSigner: PublicKey;
+  version: bigint;
+  superAdmin: PublicKey;
+  admins: PublicKey[];
+  adminPermissions: BigInt[];
+  defaultDripFeeBps: bigint;
+  globalConfigSigner: PublicKey;
 }
 
 export class Drip {
-    private readonly program;
+  private readonly program;
 
-    public constructor(
-        public readonly programId: PublicKey,
-        public readonly providerOrConnection: AnchorProvider | Connection
-    ) {
-        const provider =
-            providerOrConnection instanceof AnchorProvider
-                ? providerOrConnection
-                : new anchor.AnchorProvider(
-                      providerOrConnection,
-                      new NodeWallet(Keypair.generate()),
-                      { commitment: providerOrConnection.commitment }
-                  );
+  public constructor(
+    public readonly programId: PublicKey,
+    public readonly providerOrConnection: AnchorProvider | Connection
+  ) {
+    const provider =
+      providerOrConnection instanceof AnchorProvider
+        ? providerOrConnection
+        : new anchor.AnchorProvider(
+            providerOrConnection,
+            new NodeWallet(Keypair.generate()),
+            { commitment: providerOrConnection.commitment }
+          );
 
-        this.program = new anchor.Program<DripV2>(
-            IDL,
-            this.programId,
-            provider
-        );
+    this.program = new anchor.Program<DripV2>(IDL, this.programId, provider);
+  }
+
+  public async fetchGlobalConfig(key: PublicKey): Promise<GlobalConfigAccount> {
+    const account = await this.program.account.globalConfig.fetch(key);
+
+    return {
+      version: BigInt(account.version.toString()),
+      superAdmin: account.superAdmin,
+      admins: account.admins,
+      adminPermissions: account.adminPermissions.map((permission) =>
+        BigInt(permission.toString())
+      ),
+      defaultDripFeeBps: BigInt(account.defaultDripFeeBps.toString()),
+      globalConfigSigner: account.globalConfigSigner,
+    };
+  }
+
+  public async initGlobalConfig(
+    superAdmin: PublicKey,
+    defaultDripFeeBps: BigInt,
+    globalConfigPubkey?: PublicKey,
+    payer?: PublicKey
+  ): Promise<{ tx: Transaction; globalConfigPubkey: PublicKey }> {
+    const globalConfigKeypair = Keypair.generate();
+    const _globalConfigPubkey =
+      globalConfigPubkey ?? globalConfigKeypair.publicKey;
+
+    const [globalConfigSigner] = PublicKey.findProgramAddressSync(
+      [Buffer.from("drip-v2-global-signer"), _globalConfigPubkey.toBuffer()],
+      this.program.programId
+    );
+
+    let txBuilder = this.program.methods
+      .initGlobalConfig({
+        superAdmin: superAdmin,
+        defaultDripFeeBps: new anchor.BN(defaultDripFeeBps.toString()),
+      })
+      .accounts({
+        payer: payer ?? this.program.provider.publicKey,
+        globalConfig: _globalConfigPubkey,
+        systemProgram: SystemProgram.programId,
+        globalConfigSigner,
+      });
+
+    if (!globalConfigPubkey) {
+      txBuilder = txBuilder.signers([globalConfigKeypair]);
     }
 
-    public async fetchGlobalConfig(
-        key: PublicKey
-    ): Promise<GlobalConfigAccount> {
-        const account = await this.program.account.globalConfig.fetch(key);
-
-        return {
-            version: BigInt(account.version.toString()),
-            superAdmin: account.superAdmin,
-            admins: account.admins,
-            adminPermissions: account.adminPermissions.map((permission) =>
-                BigInt(permission.toString())
-            ),
-            defaultDripFeeBps: BigInt(account.defaultDripFeeBps.toString()),
-            globalConfigSigner: account.globalConfigSigner,
-        };
-    }
-
-    public async initGlobalConfig(
-        superAdmin: PublicKey,
-        defaultDripFeeBps: BigInt,
-        globalConfigPubkey?: PublicKey,
-        payer?: PublicKey
-    ): Promise<{ tx: Transaction; globalConfigPubkey: PublicKey }> {
-        const globalConfigKeypair = Keypair.generate();
-        const _globalConfigPubkey =
-            globalConfigPubkey ?? globalConfigKeypair.publicKey;
-
-        const [globalConfigSigner] = PublicKey.findProgramAddressSync(
-            [
-                Buffer.from("drip-v2-global-signer"),
-                _globalConfigPubkey.toBuffer(),
-            ],
-            this.program.programId
-        );
-
-        let txBuilder = this.program.methods
-            .initGlobalConfig({
-                superAdmin: superAdmin,
-                defaultDripFeeBps: new anchor.BN(defaultDripFeeBps.toString()),
-            })
-            .accounts({
-                payer: payer ?? this.program.provider.publicKey,
-                globalConfig: _globalConfigPubkey,
-                systemProgram: SystemProgram.programId,
-                globalConfigSigner,
-            });
-
-        if (!globalConfigPubkey) {
-            txBuilder = txBuilder.signers([globalConfigKeypair]);
-        }
-
-        return {
-            tx: await txBuilder.transaction(),
-            globalConfigPubkey: _globalConfigPubkey,
-        };
-    }
+    return {
+      tx: await txBuilder.transaction(),
+      globalConfigPubkey: _globalConfigPubkey,
+    };
+  }
 }
--- solana-programs/tests/program/updateAdmin.ts
+++ solana-programs/tests/program/updateAdmin.ts
@@ -1,213 +1,205 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Keypair, PublicKey, SystemProgram } from "@solana/web3.js";
 import { Program } from "@coral-xyz/anchor";
-import { DripV2 } from "../../target/types/drip_v2";
+import { DripV2 } from "@dcaf/drip-types";
 import { expect } from "chai";
 import "../setup";
 
 // TODO: These tests are not exhaustive at all yet
 
 describe("Program - updateAdmin", () => {
-    anchor.setProvider(anchor.AnchorProvider.env());
-    const program = anchor.workspace.DripV2 as Program<DripV2>;
+  anchor.setProvider(anchor.AnchorProvider.env());
+  const program = anchor.workspace.DripV2 as Program<DripV2>;
 
-    let globalConfigPubkey: PublicKey;
-    let superAdminKeypair: Keypair;
+  let globalConfigPubkey: PublicKey;
+  let superAdminKeypair: Keypair;
 
-    beforeEach(async () => {
-        const globalConfigKeypair = new Keypair();
-        superAdminKeypair = new Keypair();
+  beforeEach(async () => {
+    const globalConfigKeypair = new Keypair();
+    superAdminKeypair = new Keypair();
 
-        await program.methods
-            .initGlobalConfig({
-                superAdmin: superAdminKeypair.publicKey,
-                defaultDripFeeBps: new anchor.BN(100),
-            })
-            .accounts({
-                payer: program.provider.publicKey,
-                globalConfig: globalConfigKeypair.publicKey,
-                systemProgram: SystemProgram.programId,
-            })
-            .signers([globalConfigKeypair])
-            .rpc();
+    await program.methods
+      .initGlobalConfig({
+        superAdmin: superAdminKeypair.publicKey,
+        defaultDripFeeBps: new anchor.BN(100),
+      })
+      .accounts({
+        payer: program.provider.publicKey,
+        globalConfig: globalConfigKeypair.publicKey,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([globalConfigKeypair])
+      .rpc();
 
-        globalConfigPubkey = globalConfigKeypair.publicKey;
+    globalConfigPubkey = globalConfigKeypair.publicKey;
+  });
+
+  it("doesn't allow non-super admin to update admins", async () => {
+    await expect(
+      program.methods
+        .updateAdmin({
+          adminIndex: new anchor.BN(0),
+          adminChange: {
+            setAdminAndResetPermissions: { 0: PublicKey.default },
+          },
+        })
+        .accounts({
+          signer: superAdminKeypair.publicKey,
+          globalConfig: globalConfigPubkey,
+        })
+        .signers([superAdminKeypair])
+        .rpc()
+    ).to.eventually.be.rejectedWith(/AdminPubkeyCannotBeDefault.*6002/);
+  });
+
+  it("can add a new admin", async () => {
+    const newAdminPubkey = PublicKey.unique();
+
+    const globalConfigAccountBefore = await program.account.globalConfig.fetch(
+      globalConfigPubkey
+    );
+
+    expect({
+      version: globalConfigAccountBefore.version.toString(),
+      superAdmin: globalConfigAccountBefore.superAdmin.toString(),
+      admins: globalConfigAccountBefore.admins.map((admin) => admin.toString()),
+      adminPermissions: globalConfigAccountBefore.adminPermissions.map(
+        (admin) => admin.toString()
+      ),
+      defaultDripFeeBps: globalConfigAccountBefore.defaultDripFeeBps.toString(),
+    }).to.deep.equal({
+      version: "0",
+      superAdmin: superAdminKeypair.publicKey.toBase58(),
+      admins: Array<PublicKey>(20)
+        .fill(PublicKey.default)
+        .map((key) => key.toString()),
+      adminPermissions: Array(20).fill("0"),
+      defaultDripFeeBps: "100",
     });
 
-    it("doesn't allow non-super admin to update admins", async () => {
-        await expect(
-            program.methods
-                .updateAdmin({
-                    adminIndex: new anchor.BN(0),
-                    adminChange: {
-                        setAdminAndResetPermissions: { 0: PublicKey.default },
-                    },
-                })
-                .accounts({
-                    signer: superAdminKeypair.publicKey,
-                    globalConfig: globalConfigPubkey,
-                })
-                .signers([superAdminKeypair])
-                .rpc()
-        ).to.eventually.be.rejectedWith(/AdminPubkeyCannotBeDefault.*6002/);
+    await program.methods
+      .updateAdmin({
+        adminIndex: new anchor.BN(0),
+        adminChange: {
+          setAdminAndResetPermissions: {
+            0: newAdminPubkey,
+          },
+        },
+      })
+      .accounts({
+        signer: superAdminKeypair.publicKey,
+        globalConfig: globalConfigPubkey,
+      })
+      .signers([superAdminKeypair])
+      .rpc();
+
+    const globalConfigAccountAfter = await program.account.globalConfig.fetch(
+      globalConfigPubkey
+    );
+
+    expect({
+      version: globalConfigAccountAfter.version.toString(),
+      superAdmin: globalConfigAccountAfter.superAdmin.toString(),
+      admins: globalConfigAccountAfter.admins.map((admin) => admin.toString()),
+      adminPermissions: globalConfigAccountAfter.adminPermissions.map((perm) =>
+        perm.toString()
+      ),
+      defaultDripFeeBps: globalConfigAccountAfter.defaultDripFeeBps.toString(),
+    }).to.deep.equal({
+      version: "0",
+      superAdmin: superAdminKeypair.publicKey.toBase58(),
+      admins: [newAdminPubkey.toBase58()].concat(
+        Array<PublicKey>(19)
+          .fill(PublicKey.default)
+          .map((key) => key.toString())
+      ),
+      adminPermissions: Array(20).fill("0"),
+      defaultDripFeeBps: "100",
+    });
+  });
+
+  it("can give drip permissions to an existing admin", async () => {
+    const newAdminPubkey = PublicKey.unique();
+
+    const globalConfigAccountBefore = await program.account.globalConfig.fetch(
+      globalConfigPubkey
+    );
+
+    expect({
+      version: globalConfigAccountBefore.version.toString(),
+      superAdmin: globalConfigAccountBefore.superAdmin.toString(),
+      admins: globalConfigAccountBefore.admins.map((admin) => admin.toString()),
+      adminPermissions: globalConfigAccountBefore.adminPermissions.map(
+        (admin) => admin.toString()
+      ),
+      defaultDripFeeBps: globalConfigAccountBefore.defaultDripFeeBps.toString(),
+    }).to.deep.equal({
+      version: "0",
+      superAdmin: superAdminKeypair.publicKey.toBase58(),
+      admins: Array<PublicKey>(20)
+        .fill(PublicKey.default)
+        .map((key) => key.toString()),
+      adminPermissions: Array(20).fill("0"),
+      defaultDripFeeBps: "100",
     });
 
-    it("can add a new admin", async () => {
-        const newAdminPubkey = PublicKey.unique();
+    let tx = await program.methods
+      .updateAdmin({
+        adminIndex: new anchor.BN(0),
+        adminChange: {
+          setAdminAndResetPermissions: { 0: newAdminPubkey },
+        },
+      })
+      .accounts({
+        signer: superAdminKeypair.publicKey,
+        globalConfig: globalConfigPubkey,
+      })
+      .transaction();
 
-        const globalConfigAccountBefore =
-            await program.account.globalConfig.fetch(globalConfigPubkey);
+    const giveAdminInitPairConfigPermissionIX = await program.methods
+      .updateAdmin({
+        adminIndex: new anchor.BN(0),
+        adminChange: {
+          addPermission: { 0: { drip: {} } },
+        },
+      })
+      .accounts({
+        signer: superAdminKeypair.publicKey,
+        globalConfig: globalConfigPubkey,
+      })
+      .instruction();
 
-        expect({
-            version: globalConfigAccountBefore.version.toString(),
-            superAdmin: globalConfigAccountBefore.superAdmin.toString(),
-            admins: globalConfigAccountBefore.admins.map((admin) =>
-                admin.toString()
-            ),
-            adminPermissions: globalConfigAccountBefore.adminPermissions.map(
-                (admin) => admin.toString()
-            ),
-            defaultDripFeeBps:
-                globalConfigAccountBefore.defaultDripFeeBps.toString(),
-        }).to.deep.equal({
-            version: "0",
-            superAdmin: superAdminKeypair.publicKey.toBase58(),
-            admins: Array<PublicKey>(20)
-                .fill(PublicKey.default)
-                .map((key) => key.toString()),
-            adminPermissions: Array(20).fill("0"),
-            defaultDripFeeBps: "100",
-        });
+    tx = tx.add(giveAdminInitPairConfigPermissionIX);
+    tx.recentBlockhash = (
+      await program.provider.connection.getLatestBlockhash()
+    ).blockhash;
+    tx.feePayer = anchor.getProvider().publicKey;
+    tx.partialSign(superAdminKeypair);
 
-        await program.methods
-            .updateAdmin({
-                adminIndex: new anchor.BN(0),
-                adminChange: {
-                    setAdminAndResetPermissions: {
-                        0: newAdminPubkey,
-                    },
-                },
-            })
-            .accounts({
-                signer: superAdminKeypair.publicKey,
-                globalConfig: globalConfigPubkey,
-            })
-            .signers([superAdminKeypair])
-            .rpc();
+    await anchor.getProvider().sendAndConfirm(tx);
 
-        const globalConfigAccountAfter =
-            await program.account.globalConfig.fetch(globalConfigPubkey);
+    const globalConfigAccountAfter = await program.account.globalConfig.fetch(
+      globalConfigPubkey
+    );
 
-        expect({
-            version: globalConfigAccountAfter.version.toString(),
-            superAdmin: globalConfigAccountAfter.superAdmin.toString(),
-            admins: globalConfigAccountAfter.admins.map((admin) =>
-                admin.toString()
-            ),
-            adminPermissions: globalConfigAccountAfter.adminPermissions.map(
-                (perm) => perm.toString()
-            ),
-            defaultDripFeeBps:
-                globalConfigAccountAfter.defaultDripFeeBps.toString(),
-        }).to.deep.equal({
-            version: "0",
-            superAdmin: superAdminKeypair.publicKey.toBase58(),
-            admins: [newAdminPubkey.toBase58()].concat(
-                Array<PublicKey>(19)
-                    .fill(PublicKey.default)
-                    .map((key) => key.toString())
-            ),
-            adminPermissions: Array(20).fill("0"),
-            defaultDripFeeBps: "100",
-        });
+    expect({
+      version: globalConfigAccountAfter.version.toString(),
+      superAdmin: globalConfigAccountAfter.superAdmin.toString(),
+      admins: globalConfigAccountAfter.admins.map((admin) => admin.toString()),
+      adminPermissions: globalConfigAccountAfter.adminPermissions.map((perm) =>
+        perm.toString()
+      ),
+      defaultDripFeeBps: globalConfigAccountAfter.defaultDripFeeBps.toString(),
+    }).to.deep.equal({
+      version: "0",
+      superAdmin: superAdminKeypair.publicKey.toBase58(),
+      admins: [newAdminPubkey.toBase58()].concat(
+        Array<PublicKey>(19)
+          .fill(PublicKey.default)
+          .map((key) => key.toString())
+      ),
+      adminPermissions: ["1"].concat(Array(19).fill("0")),
+      defaultDripFeeBps: "100",
     });
-
-    it("can give drip permissions to an existing admin", async () => {
-        const newAdminPubkey = PublicKey.unique();
-
-        const globalConfigAccountBefore =
-            await program.account.globalConfig.fetch(globalConfigPubkey);
-
-        expect({
-            version: globalConfigAccountBefore.version.toString(),
-            superAdmin: globalConfigAccountBefore.superAdmin.toString(),
-            admins: globalConfigAccountBefore.admins.map((admin) =>
-                admin.toString()
-            ),
-            adminPermissions: globalConfigAccountBefore.adminPermissions.map(
-                (admin) => admin.toString()
-            ),
-            defaultDripFeeBps:
-                globalConfigAccountBefore.defaultDripFeeBps.toString(),
-        }).to.deep.equal({
-            version: "0",
-            superAdmin: superAdminKeypair.publicKey.toBase58(),
-            admins: Array<PublicKey>(20)
-                .fill(PublicKey.default)
-                .map((key) => key.toString()),
-            adminPermissions: Array(20).fill("0"),
-            defaultDripFeeBps: "100",
-        });
-
-        let tx = await program.methods
-            .updateAdmin({
-                adminIndex: new anchor.BN(0),
-                adminChange: {
-                    setAdminAndResetPermissions: { 0: newAdminPubkey },
-                },
-            })
-            .accounts({
-                signer: superAdminKeypair.publicKey,
-                globalConfig: globalConfigPubkey,
-            })
-            .transaction();
-
-        const giveAdminInitPairConfigPermissionIX = await program.methods
-            .updateAdmin({
-                adminIndex: new anchor.BN(0),
-                adminChange: {
-                    addPermission: { 0: { drip: {} } },
-                },
-            })
-            .accounts({
-                signer: superAdminKeypair.publicKey,
-                globalConfig: globalConfigPubkey,
-            })
-            .instruction();
-
-        tx = tx.add(giveAdminInitPairConfigPermissionIX);
-        tx.recentBlockhash = (
-            await program.provider.connection.getLatestBlockhash()
-        ).blockhash;
-        tx.feePayer = anchor.getProvider().publicKey;
-        tx.partialSign(superAdminKeypair);
-
-        await anchor.getProvider().sendAndConfirm(tx);
-
-        const globalConfigAccountAfter =
-            await program.account.globalConfig.fetch(globalConfigPubkey);
-
-        expect({
-            version: globalConfigAccountAfter.version.toString(),
-            superAdmin: globalConfigAccountAfter.superAdmin.toString(),
-            admins: globalConfigAccountAfter.admins.map((admin) =>
-                admin.toString()
-            ),
-            adminPermissions: globalConfigAccountAfter.adminPermissions.map(
-                (perm) => perm.toString()
-            ),
-            defaultDripFeeBps:
-                globalConfigAccountAfter.defaultDripFeeBps.toString(),
-        }).to.deep.equal({
-            version: "0",
-            superAdmin: superAdminKeypair.publicKey.toBase58(),
-            admins: [newAdminPubkey.toBase58()].concat(
-                Array<PublicKey>(19)
-                    .fill(PublicKey.default)
-                    .map((key) => key.toString())
-            ),
-            adminPermissions: ["1"].concat(Array(19).fill("0")),
-            defaultDripFeeBps: "100",
-        });
-    });
+  });
 });
