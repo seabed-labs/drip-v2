5aafdd192f779f5875c2c102a08468345bba36f7
Mocha Dcaf <mocha@dcaf.so>
2023-06-17 21:18:36 -0400

    chore(parser): remove commented code

--- services/parser/src/controllers/pingController.ts
+++ services/parser/src/controllers/pingController.ts
@@ -1,9 +1,6 @@
-// src/users/usersController.ts
 import { Controller, Get, Route } from 'tsoa'
 import { getServerResponseCommon } from './common'
 import { PingResponse } from './types'
-// import { User } from "./user";
-// import { UsersService, UserCreationParams } from "./usersService";
 
 @Route('/')
 export class PingController extends Controller {
@@ -16,14 +13,4 @@ export class PingController extends Controller {
             },
         }
     }
-
-    // @SuccessResponse("201", "Created") // Custom success response
-    // @Post()
-    // public async createUser(
-    //   @Body() requestBody: UserCreationParams
-    // ): Promise<void> {
-    //   this.setStatus(201); // set return status 201
-    //   new UsersService().create(requestBody);
-    //   return;
-    // }
 }
