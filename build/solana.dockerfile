4fda1005e4b2070b9592730d48de8a254123a396
Mocha <mocha@dcaf.so>
2023-06-22 18:26:44 -0400

    feat(root): deploy scripts for localnet

--- build/solana.dockerfile
+++ build/solana.dockerfile
@@ -1,16 +1,34 @@
 # 0.27.0 and below is under project serum
 FROM projectserum/build:v0.27.0
+
 # 0.28.0 and later are under backpackapp
 # FROM backpackapp/build:v0.28.0
 
-COPY ./solana-programs/local.json ./
-COPY ./solana-programs/Cargo.lock ./
-COPY ./solana-programs/Cargo.toml ./
-COPY ./solana-programs/Anchor.toml ./
-COPY ./solana-programs/programs ./programs/
-RUN anchor build
+WORKDIR /project
 
-COPY ./solana-programs/scripts ./scripts
+COPY solana-programs/local.json solana-programs/
+COPY solana-programs/Cargo.lock solana-programs/
+COPY solana-programs/Cargo.toml solana-programs/
+COPY solana-programs/Anchor.toml solana-programs/
+COPY solana-programs/programs solana-programs/programs/
+RUN cd solana-programs && anchor build
+
+COPY tsconfig.json .
+COPY package.json .
+COPY yarn.lock .
+RUN yarn install --pure-lockfile --non-interactive --cache-folder ./ycache; rm -rf ./ycache
+
+COPY packages/drip-types/package.json packages/drip-types/
+COPY packages/sdk/package.json packages/sdk/
+COPY solana-programs/package.json solana-programs/
+RUN cd solana-programs && yarn install --pure-lockfile --non-interactive --cache-folder ./ycache; rm -rf ./ycache
+
+COPY packages/drip-types packages/drip-types
+COPY packages/sdk packages/sdk
+
+COPY solana-programs/scripts solana-programs/scripts
 ENV RUST_BACKTRACE=1
 
-ENTRYPOINT [ "node", "./scripts/localnet.js" ]
+WORKDIR /project/solana-programs
+
+CMD [ "yarn", "run", "localnet" ]
---
+++ solana-programs/scripts/localnet.ts
@@ -0,0 +1,62 @@
+import * as anchor from '@coral-xyz/anchor'
+import { Keypair, PublicKey, SystemProgram } from '@solana/web3.js'
+import { Program } from '@coral-xyz/anchor'
+import { DripV2 } from '@dcaf/drip-types'
+import { spawn } from 'node:child_process';
+
+const localnet = spawn('anchor', ['localnet']);
+
+async function setup() {
+  const program = anchor.workspace.DripV2 as Program<DripV2>
+  const globalConfigKeypair = new Keypair()
+  const superAdmin = new Keypair()
+  const provider = anchor.getProvider()
+  const [globalSignerPubkey] = PublicKey.findProgramAddressSync(
+      [
+          Buffer.from('drip-v2-global-signer'),
+          globalConfigKeypair.publicKey.toBuffer(),
+      ],
+      program.programId
+  )
+  await program.methods
+      .initGlobalConfig({
+          superAdmin: superAdmin.publicKey,
+          defaultDripFeeBps: new anchor.BN(100),
+      })
+      .accounts({
+          payer: provider.publicKey,
+          globalConfig: globalConfigKeypair.publicKey,
+          systemProgram: SystemProgram.programId,
+          globalConfigSigner: globalSignerPubkey,
+      })
+      .signers([globalConfigKeypair])
+      .rpc()
+  console.log(globalConfigKeypair.publicKey.toString())
+}
+
+// Propagate SIGINT to the child process
+process.on('SIGINT', () => {
+  localnet.kill('SIGINT');
+});
+
+localnet.stdout.on('data', (data: any) => {
+  const logData = `${data}`
+  if (logData && !logData.includes("Processed Slot")) {
+    console.log(logData)
+  }
+  if (logData?.includes("JSON RPC URL")) {
+    setup().then(() => {
+      console.log("DONE SETUP")
+    })
+  }
+});
+
+localnet.stderr.on('data', (data: any) => {
+  console.error(`${data}`);
+});
+
+localnet.on('close', (code: any) => {
+  console.log(`child process exited with code ${code}`);
+});
+
+
