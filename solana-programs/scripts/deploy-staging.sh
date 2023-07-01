b3e7af3ad456c92a26721f765350d9826b21d0a0
Mocha Dcaf <mocha@dcaf.so>
2023-07-01 12:10:29 -0400

    feat(solana-programs): always use anchor 0.27.0 when deploying

--- solana-programs/scripts/deploy-staging.sh
+++ solana-programs/scripts/deploy-staging.sh
@@ -1,3 +1,5 @@
+avm use 0.27.0
+
 solana config set -u "https://mainnet.helius-rpc.com/?api-key=7a4bbcd8-5147-4a6b-b087-7be92b65170d"
 
 CODE_DEFINED_PROGRAM_ID="74XYB4agZ83msRxmTGvNDc8D2z8T55mfGfz3FAneNSKk"
@@ -33,7 +35,7 @@ echo $PROGRAM_ID
 echo "Upgrade Authority"
 echo $UPGRADE_AUTH
 
-anchor upgrade --program-id="${PROGRAM_ID}" --provider.cluster="https://mainnet.helius-rpc.com/?api-key=7a4bbcd8-5147-4a6b-b087-7be92b65170d" --provider.wallet="./upgrade-authority.json" ./target/deploy/drip_v2.so
+anchor upgrade --program-id="./target/deploy/drip_v2-keypair.json" --provider.cluster="https://mainnet.helius-rpc.com/?api-key=7a4bbcd8-5147-4a6b-b087-7be92b65170d" --provider.wallet="./upgrade-authority.json" ./target/deploy/drip_v2.so
 anchor idl upgrade --provider.cluster="https://mainnet.helius-rpc.com/?api-key=7a4bbcd8-5147-4a6b-b087-7be92b65170d" --provider.wallet="./upgrade-authority.json" --filepath="target/idl/drip.json" $PROGRAM_ID
 
 #solana program deploy --keypair ./upgrade-authority.json --upgrade-authority ./upgrade-authority.json --program-id ./target/deploy/drip_v2-keypair.json ./target/deploy/drip_v2.so
