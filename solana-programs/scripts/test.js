1ca355fd5ed00d15b1f456d3a2fab681ca168636
matcha <matcha@dcaf.so>
2023-07-01 13:07:33 -0400

    feat(program): add ability to run tests in sequential mode for .only support

---
+++ solana-programs/scripts/test.js
@@ -0,0 +1,19 @@
+import { spawn } from 'node:child_process';
+import process from 'node:process';
+import console from 'node:console';
+
+let testCommand =
+    'yarn run ts-mocha -p ./tsconfig.json -t 1000000 --parallel tests/**/*.ts';
+if (process.env.TEST_MODE === 'debug') {
+    testCommand =
+        'yarn run ts-mocha -p ./tsconfig.json -t 1000000 tests/**/*.ts';
+}
+
+console.log('Running:', testCommand);
+const [command, ...args] = testCommand.split(' ');
+
+spawn(command, args, {
+    cwd: process.cwd(),
+    detached: false,
+    stdio: 'inherit',
+});
