20565d87eb78d584646a17ef4cda6847650d1dac
Mocha Dcaf <mocha@dcaf.so>
2023-07-19 22:18:42 -0400

    feat: upgrade to new codegen

--- packages/drip-types/src/index.ts
+++
@@ -1,9 +0,0 @@
-export * as Accounts from './accounts';
-export * as Errors from './errors';
-export * as DripErrors from './errors/custom';
-export type * as DripErrorTypes from './errors/custom';
-export type * as AnchorErrorTypes from './errors/custom';
-export * as Instructions from './instructions';
-export type * as Types from './types';
-export * from './programId';
-export * from './drip_v2';
