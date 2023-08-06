5387ecd98adf74837988497dd57c62c3631e6c08
Mocha Dcaf <mocha@dcaf.so>
2023-08-06 13:33:57 -0400

    feat(root): init admin-web

---
+++ ui/web-admin/src/App.tsx
@@ -0,0 +1,12 @@
+import { ExampleComponent } from '@dcaf/components';
+import React from 'react';
+
+function App() {
+    return (
+        <div className="App">
+            <ExampleComponent text="Hello Example Component" />
+        </div>
+    );
+}
+
+export default App;
